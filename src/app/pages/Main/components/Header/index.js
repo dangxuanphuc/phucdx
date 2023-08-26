6278d56679a104f6df7dbc57e293655f7e38d1f1
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2023-08-26 21:33:15 +0700

    Fix toggle menu on SP

--- src/app/pages/Main/components/Header/index.js
+++ src/app/pages/Main/components/Header/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = (event) => {
+    setIsActive((current) => !current);
+  };
+
+  const closeMenu = (event) => {
+    setIsActive(false);
+  };
+
   return (
     <header className='header'>
       <nav className='nav'>
@@ -9,7 +20,7 @@ const Header = () => {
             Phuc Dang Xuan
           </a>
         </div>
-        <div className='nav__menu'>
+        <div className={isActive ? 'nav__menu nav__menu--active' : 'nav__menu'}>
           <ul className='nav__list'>
             <li className='nav__item'>
               <a href='/' className='nav__link'>
@@ -21,14 +32,14 @@ const Header = () => {
                 Skills
               </a>
             </li> */}
-            <li className='nav__item' id='link__timeline'>
+            <li className='nav__item' id='link__timeline' onClick={closeMenu}>
               <a href='#timeline' className='nav__link'>
                 Timeline
               </a>
             </li>
           </ul>
         </div>
-        <div className='nav__menu-toogle'>
+        <div className='nav__menu-toogle' onClick={toggleMenu}>
           <i className='fas fa-bars'></i>
         </div>
       </nav>
