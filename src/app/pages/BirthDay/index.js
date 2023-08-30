263ad6b830a7b74b01c6675e8246ad4ebeab5337
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2023-08-30 22:18:11 +0700

    Add birthday and page list

---
+++ src/app/pages/BirthDay/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+const BirthDay = () => {
+  return (
+    <iframe
+      src="https://tanjoubi-omedetou.netlify.app/"
+      title="Lan Chan's Birthday"
+      className="birthday-frame"
+      frameBorder="0"
+    ></iframe>
+  );
+};
+
+export default BirthDay;
